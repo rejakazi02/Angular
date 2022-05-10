24bb1fa5c814d55e15354af423594ca9aeb53b9f
Reja Kazi <rejakazi02@gmail.com>
2022-05-10 16:39:03 +0600

    Email Service

--- Introduction/hello-world/src/app/app.module.ts
+++ Introduction/hello-world/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './Courses.Component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
