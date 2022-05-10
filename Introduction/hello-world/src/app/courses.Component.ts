1064ba876b0652288ceac5f8868a550c76941e4f
Reja Kazi <rejakazi02@gmail.com>
2022-05-10 17:01:46 +0600

    Course title showing using function

--- Introduction/hello-world/src/app/courses.Component.ts
+++ Introduction/hello-world/src/app/courses.Component.ts
@@ -3,10 +3,15 @@ import { Component } from "@angular/core";
 @Component({
 
   selector: 'courses',
-  template: '<h2>{{"Title: " +  title  }}</h2>'
+  template: '<h2>{{"Title: " +  getTitle()  }}</h2>'
 })
 
 export class CoursesComponent {
 title = " List of courses";
+
+getTitle() {
+  return this.title; 
+}
+
 
 }
