42f0f531003a52abb3e34acbcc1e74747d19bee3
Reja Kazi <rejakazi02@gmail.com>
2022-05-10 16:58:44 +0600

    List of courses

--- Introduction/hello-world/src/app/courses.Component.ts
+++ Introduction/hello-world/src/app/courses.Component.ts
@@ -3,10 +3,10 @@ import { Component } from "@angular/core";
 @Component({
 
   selector: 'courses',
-  template: '<h2>Courses</h2>'
+  template: '<h2>{{"Title: " +  title  }}</h2>'
 })
 
 export class CoursesComponent {
-
+title = " List of courses";
 
 }
