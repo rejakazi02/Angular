50028bd503dd57b2fba07c72e0703939900bb3b0
Reja Kazi <rejakazi02@gmail.com>
2022-05-10 17:13:52 +0600

    List of courses using loop

--- Introduction/hello-world/src/app/courses.Component.ts
+++ Introduction/hello-world/src/app/courses.Component.ts
@@ -3,15 +3,23 @@ import { Component } from "@angular/core";
 @Component({
 
   selector: 'courses',
-  template: '<h2>{{"Title: " +  getTitle()  }}</h2>'
+  template:
+
+  `
+
+  <h2>{{"Title: " +  title   }}</h2>
+
+  <ul>
+
+  <li *ngFor="let course of courses">{{course}}
+  </li>
+
+  </ul>
+  `
 })
 
 export class CoursesComponent {
 title = " List of courses";
-
-getTitle() {
-  return this.title; 
-}
-
+courses = ['course1','course2']
 
 }
