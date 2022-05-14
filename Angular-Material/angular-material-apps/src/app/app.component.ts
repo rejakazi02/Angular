38fcb823e0e1436767c2e0386b934ab9ec0e1047
Reja Kazi <rejakazi02@gmail.com>
2022-05-14 14:59:53 +0600

    List of courses

--- Angular-Material/angular-material-apps/src/app/app.component.ts
+++ Angular-Material/angular-material-apps/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-material-apps';
+  // title = 'angular-material-apps';
+  courses = [1];
 }
