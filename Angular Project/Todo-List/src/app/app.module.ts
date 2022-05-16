db7167941471fc29a5a74f09158ff316b5357028
Reja Kazi <rejakazi02@gmail.com>
2022-05-16 12:31:47 +0600

    Created a TodoList

--- Angular Project/Todo-List/src/app/app.module.ts
+++ Angular Project/Todo-List/src/app/app.module.ts
@@ -3,11 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-
-
-
 
 @NgModule({
   declarations: [
@@ -15,10 +10,7 @@ import { MaterialModule } from './material/material.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MaterialModule
-
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
