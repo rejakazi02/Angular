13b98ba24a6b267d0445689e1b5fc7a6519b3dc3
Reja Kazi <rejakazi02@gmail.com>
2022-05-16 14:59:18 +0600

    Worked on create, view and remove

--- Angular Project/Todo-List/src/app/app.component.ts
+++ Angular Project/Todo-List/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Todo-List';
+  title = 'Todo List With Angular';
+lists: any[] = [];
+  addTask(item: string){
+this.lists.push({id:this.lists.length, name:item})
+console.warn(item);
+  }
+
+
+  removeTask(id: number) {
+
+    this.lists=this.lists.filter(item=>item.id!==id)
+  }
 }
