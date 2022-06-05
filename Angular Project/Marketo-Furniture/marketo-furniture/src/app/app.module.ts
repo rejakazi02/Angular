83be2cc031b2cc69bd511580f791218718e3d9c8
Reja Kazi <rejakazi02@gmail.com>
2022-06-05 14:59:38 +0600

    search btn

--- Angular Project/Marketo-Furniture/marketo-furniture/src/app/app.module.ts
+++ Angular Project/Marketo-Furniture/marketo-furniture/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-
-
-
 import { AppComponent } from './app.component';
+
+
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
@@ -14,8 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MatInputModule,
-    MatSelectModule
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatIconModule
+
 
 
 
