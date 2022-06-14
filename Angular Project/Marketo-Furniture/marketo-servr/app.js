5959ceadb7da18c734a4858efb8004ea19032069
Reja Kazi <rejakazi02@gmail.com>
2022-06-14 11:24:40 +0600

    another route

--- Angular Project/Marketo-Furniture/marketo-servr/app.js
+++ Angular Project/Marketo-Furniture/marketo-servr/app.js
@@ -1,1 +1,22 @@
-const 
+const express = require('express');
+const app = express();
+
+
+// create a server where browsers and API calls can connect 
+
+app.listen(3000, function () {
+
+    console.log("localhost:3000");
+});
+
+// create a route that we can access in the browser 
+
+app.get('/', function (req, res) {
+    res.send("yep it's working");    
+});
+
+// another route 
+
+app.get('/love', (req, res) => {
+    res.send('Hi love');
+})
