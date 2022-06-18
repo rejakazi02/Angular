2c7a4f7633a4cb43158286c7115a24b845012914
Reja Kazi <rejakazi02@gmail.com>
2022-06-18 13:17:22 +0600

    connection successfull

--- Angular Project/Server/basicServer/db.js
+++ Angular Project/Server/basicServer/db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb:localhost:27017/meanDB', (err)=> {
+mongoose.connect('mongodb://localhost:27017/meanDB', (err)=> {
 
     if (!err) {
-        console.log("Connection Successfull");
+        console.log(" Data Connection Successfull")
     }
     else{
-        console.log("Error in Connection" +err)
+        console.log("Error in Connection")
     }
-});
+})
 
 module.exports = mongoose;
 
