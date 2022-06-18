a8d3b442d7e287516a25a678eb58681bcfc2d882
Reja Kazi <rejakazi02@gmail.com>
2022-06-18 13:36:53 +0600

    rr

--- Angular Project/Server/basicServer/index.js
+++ Angular Project/Server/basicServer/index.js
@@ -1,1 +1,14 @@
 
+const exprass = require('exprass');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('./db.js');
+
+
+
+
+const app = exprass();
+
+app.use(bodyParser.json());
+app.use(cors({origin:'http://localhost:4200'}));
+app.listen(3000, () => console.log('Server Started at Port: 3000'));
