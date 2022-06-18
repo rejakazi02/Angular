7d19970f016ba769254ae4b8e27532b778bc6827
Reja Kazi <rejakazi02@gmail.com>
2022-06-18 12:48:53 +0600

    server create

---
+++ Angular Project/Server/basicServer/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb:localhost:27017/meanDB', (err)=> {
+
+    if (!err) {
+        console.log("Connection Successfull");
+    }
+    else{
+        console.log("Error in Connection" +err)
+    }
+});
+
+module.exports = mongoose;
+
+
+
