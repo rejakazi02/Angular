78bdd53e14d29a99d0ee75b70509af638df5542d
Reja Kazi <rejakazi02@gmail.com>
2022-06-19 17:16:57 +0600

    get data with single data

--- Angular Project/Server/basicServer/index.js
+++ Angular Project/Server/basicServer/index.js
@@ -1,20 +1,15 @@
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('./db.js');
-const router = require('./route/route.js')
-
-
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("./db.js");
+const router = require("./route/route.js");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors({origin:'http://localhost:4200'}));
-app.listen(3000, () => console.log('Server Started at Port: 3000'));
-app.use('/employee', router);
-
+app.use(cors({ origin: "http://localhost:4200" }));
+app.listen(3000, () => console.log("Server Started at Port: 3000"));
+app.use("/employee", router);
 
 
 
--- Angular Project/Server/basicServer/route/route.js
+++ Angular Project/Server/basicServer/route/route.js
@@ -1,29 +1,87 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Employee = require('../models/employee');
+const Employee = require("../models/employee");
+const ObjectId = require("mongoose").Types.ObjectId;
+
+// get api
+
+router.get("/get", (req, res) => {
+  Employee.find((err, doc) => {
+    if (err) {
+      console.log("Error in post datad" + err);
+    } else {
+      res.send(doc);
+      console.log("Get the Data SuccessFully");
+    }
+  });
+});
+
+//   get single api
+
+router.get("/put/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    Employee.findById(req.params.id, (err, doc) => {
+      if (err) {
+        console.log("Error in post datad" + err);
+      } else {
+        res.send(doc);
+        console.log("put the Data SuccessFully");
+      }
+    });
+  } else {
+    return res.status(400).send("No data find by id" + req.params.id);
+  }
+});
+// router.put("/put/:id", (req, res) => {
+//     Employee.findByIdAndUpdate(req.params.id, {
+//         name: req.body.name,
+//         position: req.body.position,
+//         dept: req.body.dept,
+
+//     }, (err, doc) => {
+//         if (err) {
+//           console.log("Error in post datad" + err);
+//         } else {
+//           res.send(doc);
+//           console.log("put the Data SuccessFully")
+//         }
+//     })
+
+//     });
 
 
+// delete api----------------
 
-router.post('/', (req, res) => {
-    let emp = new Employee({
-        name : req.body.name,
-        position : req.body.position,
-        dept : req.body.dept
-    })
+router.get("/put/:id", (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+      Employee.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (err) {
+          console.log("Error in Delete data" + err);
+        } else {
+          res.send(doc);
+          console.log("put the Data SuccessFully");
+        }
+      });
+    } else {
+      return res.status(400).send("No data find by id" + req.params.id);
+    }
+  });
 
-emp.save((err, doc) => {
+// post api
+router.post("/", (req, res) => {
+  let emp = new Employee({
+    name: req.body.name,
+    position: req.body.position,
+    dept: req.body.dept,
+  });
 
-if (err){
-    console.log('Error in post datad' +err)
-}
-else {
-
-    res.send(doc);
-}
-
-})
+  emp.save((err, doc) => {
+    if (err) {
+      console.log("Error in post datad" + err);
+    } else {
+      res.send(doc);
+    }
+  });
 });
 
 module.exports = router;
- 
