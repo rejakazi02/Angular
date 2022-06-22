20c1903fecf821ffa22df4da97ba5bdfd2c65abe
Reja Kazi <rejakazi02@gmail.com>
2022-06-22 15:23:27 +0600

    side nav

--- Angular Project/MEAN Stack/realApp/src/app/invoice-builder/invoice-builder-routing.module.ts
+++ Angular Project/MEAN Stack/realApp/src/app/invoice-builder/invoice-builder-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoiceBuilderComponent } from './invoice-builder.component';
-
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 const routes: Routes = [
   {
     path : '',
     component : InvoiceBuilderComponent,
+
+    children: [
+      {
+        path: '',
+        component: MainContentComponent
+      }
+    ]
   }
 ];
 
