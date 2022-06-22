21d57d8e79dc409a8de2951744170506c16262b4
Reja Kazi <rejakazi02@gmail.com>
2022-06-22 14:58:37 +0600

    vb

--- Angular Project/MEAN Stack/realApp/src/app/app-routing.module.ts
+++ Angular Project/MEAN Stack/realApp/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: AppComponent
+  },
+{
+  path : 'invoice-builder',
+  loadChildren: () => import('../app/invoice-builder/invoice-builder.module').then(mod => mod.InvoiceBuilderModule),
+
+},
+
+{
+  path: '**',
+  redirectTo: 'invoice-builder'
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Angular Project/MEAN Stack/realApp/src/app/invoice-builder/invoice-builder-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { InvoiceBuilderComponent } from './invoice-builder.component';
+
+
+const routes: Routes = [
+  {
+    path : '',
+    component : InvoiceBuilderComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+
+exports: [RouterModule]
+})
+export class InvoiceBuilderRoutingModule { }
